aa73a8dff81d9daefa3cd8c20d4b0784648c9e13
leejss <goldemshine@gmail.com>
2022-09-14 21:49:40 +0900

    With prisma

---
+++ pages/add.tsx
@@ -0,0 +1,9 @@
+const AddTodoPage = () => {
+  return (
+    <div className="flex justify-center items-center mt-[60px]">
+      <input className="rounded-xl border border-slate-500 w-[50%] p-4" />
+    </div>
+  );
+};
+
+export default AddTodoPage;
